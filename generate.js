595a2dc9a2e2248d37aee276ff2a25299a83d1af
Александр <34546109+maxshow1970@users.noreply.github.com>
2019-01-08 21:41:49 +0200

    Add files via upload

---
+++ generate.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+   var faker = require("faker");
+   var _ = require("lodash");
+
+    return{
+        magazin: _.times(100, function (n) {
+            return{
+                id: n,
+                name : faker.name.lastName(),
+                size: n,
+                price : n
+
+            }
+        })
+    }
+
+}
